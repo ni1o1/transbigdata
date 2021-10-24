d17eb45569ef1cefd70ca18ec578afc158578823
余庆 <yuqing@yuqingdeMacBook-Air-4.local>
2021-10-24 22:38:00 +0800

    1.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ html_logo = "_static/logo-wordmark-light.png"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
