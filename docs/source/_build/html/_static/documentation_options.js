6fb68700b1fdd6b65068eccc2d145eff87464e62
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-01-07 16:58:30 +0800

    Update docs

--- docs/source/_build/html/_static/documentation_options.js
+++ docs/source/_build/html/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.3.0',
-    LANGUAGE: 'zh_CN',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
