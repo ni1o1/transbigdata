6d44821ad4984d4ed041c177f76b75d93f4d6156
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-01-19 23:14:08 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2021, Qing Yu'
 author = 'Qing Yu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
-version = '0.3.0'
+release = '0.3.3'
+version = '0.3.3'
 html_logo = "_static/logo-wordmark-light.png"
 html_favicon = '_static/logo2.ico'
 # -- General configuration ---------------------------------------------------
