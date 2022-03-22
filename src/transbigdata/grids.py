4edbc4a69ddebc56b7a388aa9ce3a92314e71f2c
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-03-22 22:17:41 +0800

    Update grids.py

--- src/transbigdata/grids.py
+++ src/transbigdata/grids.py
@@ -650,8 +650,6 @@ def regenerate_params(grid):
     >>> bounds = [113.6, 22.4, 113.605, 22.405]
     >>> grid,params = tbd.rect_grids(bounds,500)
     >>> tbd.regenerate_params(grid)
-    [113.60000000000001, 22.400000000000002,
-    0.004863669213932553, 0.004496605206423254]
     '''
     grid_coord = np.array(grid['geometry'].iloc[0].exterior.coords)
     loncol = grid['LONCOL'].iloc[0]
