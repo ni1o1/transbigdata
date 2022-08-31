ba03fe27baa6307df08f3902ef5c8592b3528a0e
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-08-31 10:16:35 +0800

    update tests

--- src/transbigdata/tests/test_plotmap.py
+++ src/transbigdata/tests/test_plotmap.py
@@ -8,9 +8,16 @@ class TestPlotmap:
 
     def test_plot_map(self):
         bounds = [113.6, 22.4, 114.8, 22.9]
+        tbd.set_mapboxtoken(r'pk.eyJ1IjoibmkxbzEiLCJhIjoiY2t3ZDgzMmR5NDF4czJ1cm84Z3NqOGt3OSJ9.yOYP6pxDzXzhbHfyk3uORg')
         tbd.set_imgsavepath(r'/')
         _ = plt.figure(1, (8, 8), dpi=250)
         ax = plt.subplot(111)
         tbd.plot_map(plt,bounds,zoom = 6,style = 4)
         tbd.plotscale(ax, bounds=bounds, textsize=10, compasssize=1,
+                      accuracy=2000, rect=[0.06, 0.03], zorder=10)
+        tbd.plotscale(ax, bounds=bounds,unit='M', textsize=10, compasssize=1,
+                      accuracy=2000, rect=[0.06, 0.03], zorder=10)
+        tbd.plotscale(ax, bounds=bounds, textsize=10, compasssize=1,style = 2,
+                      accuracy=2000, rect=[0.06, 0.03], zorder=10)
+        tbd.plotscale(ax, bounds=bounds,unit='M', textsize=10, compasssize=1,style = 2,
                       accuracy=2000, rect=[0.06, 0.03], zorder=10)
