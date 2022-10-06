dd2dc1940c549a534c16cdd4270eba8a5276fed4
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-10-06 09:54:33 +0800

    Update test_getbusdata.py

--- src/transbigdata/tests/test_getbusdata.py
+++ src/transbigdata/tests/test_getbusdata.py
@@ -23,8 +23,11 @@ class TestGetbusdata:
         admin = 0
         t = 0
         while (type(admin)==int)&(t<5):
-            admin, _ = tbd.getadmin(
-                '深圳市', ak='2305ee7c82c147f11aac58fcc5bb7f19',jscode = '694338a096c6c50b74e5d74f411c9ab5', subdistricts=True)
+            try:
+                admin, _ = tbd.getadmin(
+                    '深圳市', ak='2305ee7c82c147f11aac58fcc5bb7f19',jscode = '694338a096c6c50b74e5d74f411c9ab5', subdistricts=True)
+            except:
+                pass
             t+=1
         assert '深圳市' in admin['name'].sum()
 
@@ -33,10 +36,13 @@ class TestGetbusdata:
         result = 0
         t = 0
         while (type(result)==int)&(t<5):
-            result = tbd.get_isochrone_mapbox(
-                120, 30, 20,
-                access_token='pk.eyJ1IjoibHByMTIxNDc5IiwiYSI6ImNrd2c0YXVydTBremQyb3V0cHVhMml5anAifQ.Y-q937VgT0diVxukUqwofw',
-                mode='walking')
+            try:
+                result = tbd.get_isochrone_mapbox(
+                    120, 30, 20,
+                    access_token='pk.eyJ1IjoibHByMTIxNDc5IiwiYSI6ImNrd2c0YXVydTBremQyb3V0cHVhMml5anAifQ.Y-q937VgT0diVxukUqwofw',
+                    mode='walking')
+            except:
+                pass
             t+=1
         import geopandas as gpd
         assert type(result) == gpd.geodataframe.GeoDataFrame
@@ -44,9 +50,12 @@ class TestGetbusdata:
         result = 0
         t = 0
         while (type(result)==int)&(t<5):
-            result = tbd.get_isochrone_amap(
-                121.212403, 31.282477, 60, ak='2305ee7c82c147f11aac58fcc5bb7f19', jscode = '694338a096c6c50b74e5d74f411c9ab5',
-                mode=0)
+            try:
+                result = tbd.get_isochrone_amap(
+                    121.212403, 31.282477, 60, ak='2305ee7c82c147f11aac58fcc5bb7f19', jscode = '694338a096c6c50b74e5d74f411c9ab5',
+                    mode=0)
+            except:
+                pass
             t+=1
         import geopandas as gpd
         assert type(result) == gpd.geodataframe.GeoDataFrame
