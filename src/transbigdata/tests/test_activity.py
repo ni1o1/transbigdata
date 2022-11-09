be58b555eadde16fa316a89be05e069d00f42217
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-11-09 13:46:49 +0800

    Update test_activity.py

--- src/transbigdata/tests/test_activity.py
+++ src/transbigdata/tests/test_activity.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 class TestActivity:
     def test_entropy(self):
-        result = tbd.cal_entropy([1,2,3])
+        result = tbd.entropy([1,2,3])
         truth = 1.584962500721156
         assert np.allclose(result,truth)
 
     def test_entropy_rate(self):
-        result = tbd.cal_entropy_rate([1,2,3,1,2,1])
+        result = tbd.entropy_rate([1,2,3,1,2,1])
         truth = 0.5283208335737187
         assert np.allclose(result,truth)
 
