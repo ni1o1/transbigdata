3c70201a196575d905c198dc169658e153bcec87
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2023-06-05 21:01:07 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
+language = 'en'
 locale_dirs = ['../locale/']   # path is example but recommended.
 gettext_compact = False     # optional.
 
